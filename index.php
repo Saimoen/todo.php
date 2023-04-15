f5fc1685ad4f2c6ebf088b13e73c969f9bceb0e7
Gregory Saimoen <greg.eres.07@hotmail.com>
2023-04-15 19:16:22 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+        <?php require_once 'includes/head.php' ?>
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+    <div class="content">
+        <div class="todo-container">
+            <h1>Ma ToDo</h1>
+            <div class="todo-form"></div>
+            <div class="todo-list"></div>
+        </div>
+    </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+</body>
+</html>
